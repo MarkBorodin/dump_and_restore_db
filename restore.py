14012d6362fbde835ca146d2a0f14aa77c38237e
mark <rens2588@gmail.com>
2021-07-16 16:32:03 +0300

    init commit

---
+++ restore.py
@@ -0,0 +1,52 @@
+import os, sys, stat
+from distutils.dir_util import copy_tree
+import subprocess
+
+
+# db container name (I used database via Docker)
+db_container_name = 'mauticdb_test'
+
+# container_name
+container_name = 'automationmonkey_latest'
+
+# database name
+db_name = 'mautic'
+
+# user name
+user = 'root'
+
+# password
+password = 'mysecret'
+
+# path to the folder with the script
+path = os.path.dirname(os.path.abspath(__file__))
+
+# dump file name
+dump_name = 'dump.sql'
+
+# path to the folder with the script + '/media/.'
+dump_path = os.path.dirname(os.path.abspath(__file__)) + '/files/.'
+# dump_path = os.path.dirname(os.path.abspath(__file__)) + '/media/.'
+
+
+# this function takes the specified parameters and generates a command to download the dump
+def restore(db_container_name, db_name, user, password, dump_name):
+    script = f'docker exec -i {db_container_name} mysql -u{user} -p{password} {db_name} < {dump_name}'
+    os.system(script)
+
+
+def load_media(dump_path, container_name):
+    script = f'docker cp {dump_path} {container_name}:/var/www/html/media/files/'
+    # script = f'docker cp {dump_path} {container_name}:/var/www/html/media/.'
+    os.system(script)
+
+
+def remove_tmp(container_name):
+    rm_tmp = f'docker exec {container_name} rm -rf /var/www/html/media/files/tmp'
+    os.system(rm_tmp)
+
+
+if __name__ == '__main__':
+    restore(db_container_name, db_name, user, password, dump_name)
+    load_media(dump_path, container_name)
+    remove_tmp(container_name)
